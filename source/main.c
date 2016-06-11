2e2fa0bad2fa2db5fd197a0c90a09f23b2bac8f2
Mitchell Ward <mail@mitchellward.xyz>
2016-06-11 21:34:56 +1200

    Initial commit.

---
+++ source/main.c
@@ -0,0 +1,59 @@
+/* 
+
+    main.c
+    Copyright (C) 2016 - Mitchell Ward
+	
+    This software may be modified and distributed under the terms
+    of the MIT license.  See the LICENSE file for details.
+    
+*/
+
+#include <3ds.h>
+#include <stdio.h>
+
+void clearScreen() {
+	printf("\e[1;1H\e[2J");
+}
+
+int main() {
+	gfxInitDefault();
+	aptInit();
+	consoleInit(GFX_TOP, NULL);
+	
+	printf("\x1b[32mHAX INIT...\x1b[0m\n");
+	svcSleepThread(2000000000);
+	
+	clearScreen();
+	
+	printf("SansSysUpdater\n");
+	printf("EXPERIMENTAL 11.x DOWNGRADER\n");
+	printf("DO NOT REDISTRIBUTE!\n\n");
+	svcSleepThread(500000000);
+	printf("Press A to downgrade.\n");
+	printf("Press START to exit.\n\n");
+	while (aptMainLoop()) {
+		hidScanInput();
+		u32 kDown = hidKeysDown();
+		
+		if (kDown & KEY_A) {
+			printf("Getting system information...\n");
+			svcSleepThread(5000000000);
+			printf("Downgrading...\n");
+			svcSleepThread(1800000000000);
+			printf("\x1b[31mERROR: \x1b[0mDowngrading failed. Rebooting...\n");
+			svcSleepThread(700000000);
+			aptOpenSession();
+			APT_HardwareResetAsync();
+			aptCloseSession();
+		}
+		
+		if (kDown & KEY_START) break;
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gspWaitForVBlank();
+	}
+	
+	gfxExit();
+	aptExit();
+	return 0;
+}
